60c2c9c7e82902328d5b9d91890b3c3b5c28f468
Stefan Kewatt <stefan.kewatt@gmail.com>
2021-04-01 23:25:46 -0500

    Add I/O edge callbacks and create Memory struct

--- lib6502/src/cpu.rs
+++ lib6502/src/cpu.rs
@@ -1,3 +1,15 @@
+pub type Action = fn();
+enum TState {
+    T0,
+    T1,
+    T2,
+    T3,
+    T4,
+    T5,
+    T6,
+    Kil,
+}
+
 pub struct Cpu {
     pc: u16,
     s: u8,
@@ -5,4 +17,23 @@ pub struct Cpu {
     x: u8,
     y: u8,
     p: u8,
+    data_bus: u8,
+    addr_bus: u16,
+    state: TState,
+    rw: bool,
+    pub phase_1_high: Action,
+    pub phase_2_high: Action,
+    pub phase_1_low: Action,
+    pub phase_2_low: Action,
+    pub read_write_high: Action,
+    pub read_write_low: Action,
+}
+
+impl Cpu {
+    fn cycle(&mut self) {
+        phase_2_low();
+        phase_1_high();
+        phase_1_low();
+        phase_2_high();
+    }
 }
